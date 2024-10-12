2f9ed04c41432c67caae32c0a4ec5d1d23bff213
tw4 <88425310+tw4@users.noreply.github.com>
2024-10-12 13:55:45 +0300

    updata: docs

--- Nexar.Test/Nexar.Test/obj/Debug/net8.0/Nexar.Test.AssemblyInfo.cs
+++ Nexar.Test/Nexar.Test/obj/Debug/net8.0/Nexar.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nexar.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca06a279ed3b76362a0f109bc05ab3f62615ffc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+144eca36ecaaecd585aaea495f4d68ecc330c232")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nexar.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nexar.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
